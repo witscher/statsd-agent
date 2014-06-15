eaf259e093592568582d15e03f53ce183ded5ddb
Josip Lazic <josip@lazic.info>
2014-06-15 12:09:54 +0200

    minor fix

--- statsd-agent.py
+++ statsd-agent.py
@@ -8,16 +8,16 @@ host = socket.gethostname()
 server = '1.1.1.1'  #Define remote statsd server here
 port = 8125         #Define remote port here
 
-last_net_io   = psutil.net_io_counters()
+last_net   = psutil.net_io_counters()
 net_speed = {}
 statsd_client = statsd.StatsClient(server, port, prefix=host+'.')
 while True:
-    memory          = psutil.phymem_usage()
-    disk            = psutil.disk_usage("/")
-    net_io          = psutil.net_io_counters()
-    net_speed['recv'] = (net_io.bytes_recv - last_net_io.bytes_recv) / 10
-    net_speed['sent'] = (net_io.bytes_sent - last_net_io.bytes_sent) / 10
-    last_net_io     = net_io
+    memory = psutil.phymem_usage()
+    disk = psutil.disk_usage("/")
+    net = psutil.net_io_counters()
+    net_speed['recv'] = (net.bytes_recv - last_net.bytes_recv) / 10
+    net_speed['sent'] = (net.bytes_sent - last_net.bytes_sent) / 10
+    last_net = net
             
     gauges = {
         "memory.used":        memory.used,
@@ -30,10 +30,10 @@ while True:
         "disk.size.percent":  disk.percent,
         "net.in.bytes":       net_speed["recv"],
         "net.out.bytes":      net_speed["sent"],
-        "net.in.errors":      net_io.errin,
-        "net.in.dropped":     net_io.dropin,
-        "net.out.errors":     net_io.errout,
-        "net.out.dropped":    net_io.dropout,
+        "net.in.errors":      net.errin,
+        "net.in.dropped":     net.dropin,
+        "net.out.errors":     net.errout,
+        "net.out.dropped":    net.dropout,
     }
     
     for name, value in gauges.items():
