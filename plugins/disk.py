b6805e441701315f33b4eab325b93cd1b619f631
David Heidt <david@heidt.biz>
2015-02-23 15:19:23 +0100

    change parameters name

--- plugins/disk.py
+++ plugins/disk.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     import sys
     import argparse
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-d', metavar='<mount point>', default='/', help='which mountpoint to check')
+    parser.add_argument('--mountpoint', metavar='<mount point>', default='/', help='which mountpoint to check')
     args = parser.parse_args()
-    output = collect(args.d)
+    output = collect(args.mountpoint)
     for name, value in output.items():
     	print name, value
